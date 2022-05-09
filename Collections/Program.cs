f36169a0843255608d5c83afe462247d84353707
Muhammed Taha Bezci <Muhammed Taha Bezci@DESKTOP-H7IJSLK>
2022-05-09 14:28:51 +0300

    Add project files.

---
+++ Collections/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Collections
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] {"taha","engin","ayşe" };
+            //Console.WriteLine(isimler[0]);
+            //isimler = new string[3];
+            //isimler[3] = "mehmet";
+
+            List<string> isimler2 = new List<string>
+                { "taha", "engin", "ayşe"};
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+            Console.WriteLine(isimler2[2]);
+            isimler2.Add("ilker");
+            Console.WriteLine( "-----------------");
+            Console.WriteLine(  isimler2[3]);
+            Console.WriteLine(isimler2[0]);
+
+
+        }
+    }
+}
